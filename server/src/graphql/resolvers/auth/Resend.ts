972a61dd66e01f3806400f278600995d5b39169d
Zee <56460839+VireZee@users.noreply.github.com>
2025-04-01 19:57:08 +0700

    resend

--- server/src/graphql/resolvers/auth/Resend.ts
+++ server/src/graphql/resolvers/auth/Resend.ts
@@ -8,34 +8,70 @@ const Resend = async (_: null, context: { req: Request }) => {
     const t = req.cookies['!']
     try {
         const { id } = verifyToken(t)
-        const blockTTL = await Redis.ttl(`block:${id}`)
-        if (blockTTL > 0) {
-            const hours = Math.floor(blockTTL / 3600)
-            const minutes = Math.floor(blockTTL / 60)
-            const seconds = blockTTL % 60
-            const timeLeft = hours > 0
-                ? `${hours}h ${minutes}m ${seconds}s`
-                : minutes > 0
-                    ? `${minutes}m ${seconds}s`
-                    : `${seconds}s`
-            throw new GraphQLError(`Too many attempts! Try again in ${timeLeft}!`, { extensions: { code: 429 } })
+        const redisKey = `resend:${id}`
+        const resend = await Redis.exists(redisKey)
+        if (resend === 0) await Redis.hset(redisKey, 'attempts', 1)
+        else {
+            const getAttempts = await Redis.hget(redisKey, 'attempts')
+            let attempts = parseInt(getAttempts!)
+            if (attempts % 3 === 0) {
+                const blockUntil = new Date()
+                const blockDuration = 60 * 5 * (2 ** ((attempts / 3) - 1))
+                blockUntil.setSeconds(blockUntil.getSeconds() + blockDuration)
+                await Redis.hset(redisKey, 'block', blockUntil.toISOString())
+                const hours = Math.floor(blockDuration / 3600)
+                const minutes = Math.floor((blockDuration % 3600) / 60)
+                const seconds = blockDuration % 60
+                const timeLeft = hours > 0
+                    ? `${hours}h ${minutes}m ${seconds}s`
+                    : minutes > 0
+                        ? `${minutes}m ${seconds}s`
+                        : `${seconds}s`
+                throw new GraphQLError(`Too many resend attempts! Try again in ${timeLeft}!`, { extensions: { code: 429 } })
+            }
+            attempts = await Redis.hincrby(redisKey, 'attempts', 1)
         }
-        const resendTTL = await Redis.ttl(`resend:${id}`)
-        if (resendTTL > 0) {
-            const minutes = Math.floor(resendTTL / 60)
-            const seconds = resendTTL % 60
-            const timeLeft = minutes > 0 ? `${minutes}m ${seconds}s` : `${seconds}s`
-            throw new GraphQLError(`Please wait ${timeLeft} before requesting a new code!`, { extensions: { code: 429 } })
-        }
-        const attempts = await Redis.hincrby(`verify:${id}`, 'attempts', 1)
-        if (attempts >= 3) {
-            await Redis.set(`block:${id}`, "1", "EX", 30 * 60)
-            await Redis.hset(`verify:${id}`, "attempts", "0")
-            throw new GraphQLError(
-                `Too many attempts! Try again in 30 minutes.`,
-                { extensions: { code: 429 } }
-            );
-        }
+        // else if (resend exist) {
+        // const resendTTL = await Redis.ttl(`resend:${id}`)
+        // if (resendTTL > 0) {
+        //     const hours = Math.floor(resendTTL / 3600)
+        //     const minutes = Math.floor((resendTTL % 3600) / 60)
+        //     const seconds = resendTTL % 60
+        //     const timeLeft = hours > 0
+        //         ? `${hours}h ${minutes}m ${seconds}s`
+        //         : minutes > 0
+        //         ? `${minutes}m ${seconds}s`
+        //         : `${seconds}s`
+        //         throw new GraphQLError(`Please wait ${timeLeft} before requesting a new code!`, { extensions: { code: 429 } })
+        //     }
+        // }
+        // const blockTTL = await Redis.ttl(`block:${id}`)
+        // if (blockTTL > 0) {
+        //     const hours = Math.floor(blockTTL / 3600)
+        //     const minutes = Math.floor((blockTTL % 3600) / 60)
+        //     const seconds = blockTTL % 60
+        //     const timeLeft = hours > 0
+        //         ? `${hours}h ${minutes}m ${seconds}s`
+        //         : minutes > 0
+        //             ? `${minutes}m ${seconds}s`
+        //             : `${seconds}s`
+        //     throw new GraphQLError(`Too many attempts! Try again in ${timeLeft}!`, { extensions: { code: 429 } })
+        // }
+        // await Redis.hsetnx(`verify:${id}`, 'attempts', 1)
+        // const attempts = await Redis.hincrby(`verify:${id}`, 'attempts', 1)
+        // if (attempts % 3 === 0) {
+        //     const blockDuration = 60 * 30 * (2 ** ((attempts / 3) - 1))
+        //     await Redis.setex(`block:${id}`, blockDuration, '')
+        //     const hours = Math.floor(blockDuration / 3600)
+        //     const minutes = Math.floor((blockDuration % 3600) / 60)
+        //     const seconds = blockDuration % 60
+        //     const timeLeft = hours > 0
+        //         ? `${hours}h ${minutes}m ${seconds}s`
+        //         : minutes > 0
+        //             ? `${minutes}m ${seconds}s`
+        //             : `${seconds}s`
+        //     throw new GraphQLError(`Too many attempts! Try again in ${timeLeft}!`, { extensions: { code: 429 } })
+        // }
         return true
     } catch (e) {
         throw e
