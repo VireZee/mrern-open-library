19b0a933310c0237a7ed4e6b7bf7805278e09079
Vire <56460839+VireZee@users.noreply.github.com>
2025-04-16 15:51:41 +0700

    rename

--- server/src/modules/book/resolver.ts
+++ server/src/modules/book/resolver.ts
@@ -1,5 +1,5 @@
 import home from '@modules/book/resolver/home.ts'
-import fetch from '@modules/book/resolver/fetchs.ts'
+import fetch from '@modules/book/resolver/fetch.ts'
 import collection from '@modules/book/resolver/collection.ts'
 import addRemove from '@modules/book/resolver/addRemove.ts'
 
