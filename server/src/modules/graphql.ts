f436063333e305af0720720142a773186b330405
Vire <56460839+VireZee@users.noreply.github.com>
2025-04-16 16:02:42 +0700

    temp

---
+++ server/src/modules/graphql.ts
@@ -0,0 +1,27 @@
+import api from '@modules/api/resolver.ts'
+import auth from '@modules/auth/resolver.ts'
+import book from '@modules/book/resolver.ts'
+import middleware from '@modules/middleware/resolver.ts'
+
+export const typeDefs = Schema
+export const resolvers = {}
+//     Query: {
+//         // auth: Auth,
+//         home: Home,
+//         fetch: Fetch,
+//         collection: Collection,
+//         // check: Check
+//     },
+//     Mutation: {
+//         // register: Register,
+//         // verify: Verify,
+//         // resend: Resend,
+//         // login: Login,
+//         add: AddRemove,
+//         remove: AddRemove,
+//         // generate: Generate,
+//         // settings: Settings,
+//         // logout: Logout,
+//         // delete: Delete
+//     }
+// }
