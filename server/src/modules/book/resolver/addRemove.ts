b67cd19bc6e12acd1298f75f485b932c7ecf3662
Vire <56460839+VireZee@users.noreply.github.com>
2025-04-17 11:31:12 +0700

    temp

--- server/src/modules/book/resolver/addRemove.ts
+++ server/src/modules/book/resolver/addRemove.ts
@@ -1,15 +1,14 @@
 import Redis from '@database/Redis.ts'
 import collection from '@models/collection.ts'
 import type { User } from '@type/models/user.d.ts'
+import updateCollectionService from '@services/updateCollection.ts'
 import { sanitizeRedisKey } from '@utils/security/sanitizer.ts'
-import formatBooksChild from '@utils/formatter/books.ts'
 
 const addRemove = async (_: null, args: { author_key: string[], cover_edition_key: string, cover_i: number, title: string, author_name: string[] }, context: { user: User }) => {
     try {
         const { author_key, cover_edition_key, cover_i, title, author_name } = args
         const { user } = context
         const key = sanitizeRedisKey('collection', user._id)
-        const keysToDelete = sanitizeRedisKey('collection', `${user._id}|*`)
         await Redis.json.SET(key, '$', [], { NX: true })
         const bookCollection = await collection.findOne({
             user_id: user._id,
@@ -19,11 +18,7 @@ const addRemove = async (_: null, args: { author_key: string[], cover_edition_ke
         })
         if (bookCollection) {
             await collection.findByIdAndDelete(bookCollection._id)
-            const updatedBooks = await collection.find({ user_id: user._id })
-            await Redis.json.SET(key, '$', formatBooksChild(updatedBooks))
-            await Redis.EXPIRE(key, 86400)
-            const keys = await Redis.KEYS(keysToDelete)
-            if (keys.length > 0) await Redis.DEL(keys)
+            updateCollectionService('collection', user)
         }
         else {
             await collection.create({
@@ -35,11 +30,7 @@ const addRemove = async (_: null, args: { author_key: string[], cover_edition_ke
                 author_name,
                 created: new Date()
             })
-            const updatedBooks = await collection.find({ user_id: user._id })
-            await Redis.json.SET(key, '$', formatBooksChild(updatedBooks))
-            await Redis.EXPIRE(key, 86400)
-            const keys = await Redis.KEYS(keysToDelete)
-            if (keys.length > 0) await Redis.DEL(keys)
+            updateCollectionService('collection', user)
         }
         return true
     } catch (e) {
