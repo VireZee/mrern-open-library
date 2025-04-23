69118a03f41d1a239e428f6d412459ecc43984b6
Vire <56460839+VireZee@users.noreply.github.com>
2025-04-23 14:23:09 +0700

    .

--- server/src/modules/auth/resolver/forget.ts
+++ server/src/modules/auth/resolver/forget.ts
@@ -1,12 +1,9 @@
 import userModel from '@models/user.ts'
 import generateCode from '@services/verification/generateCode.ts'
-import validateEmail from '@utils/validators/email.ts'
 
 const forget = async (_: null, args: { email: string }) => {
     try {
         const { email } = args
-        const emailErr = await validateEmail(email)
-        if (emailErr) throw new GraphQLError(emailErr, { extensions: { code: 422 } })
         const user = await userModel.findOne({ email }).lean()
         if (user) await generateCode('forget', user, true)
         return 'A verification code has been sent to your email address, provided it is registered in our system.'
