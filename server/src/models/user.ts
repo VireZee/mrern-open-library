3173f294091d60f4a5999b124f6dea8fd8f7c0b7
Vire <56460839+VireZee@users.noreply.github.com>
2025-06-12 16:01:47 +0700

    google temp

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -1,12 +1,14 @@
-import mongoose, { Schema } from 'mongoose'
+import mongoose, { Schema, HydratedDocument } from 'mongoose'
 
 const userSchema = new Schema({
     photo: { type: Buffer, required: true },
     name: { type: String, maxlength: 75, required: true, },
     username: { type: String, unique: true, maxlength: 20, required: true },
     email: { type: String, unique: true, required: true },
-    pass: { type: String, required: true },
-    verified: { type: Boolean, default: false, required: true },
+    pass: { type: String, required: function (this: HydratedDocument<>) { return !this.googleId } },
+    googleId: { type: String, unique: true, sparse: true },
+    registeredWithGoogle: { type: Boolean, default: false },
+    verified: { type: Boolean, default: false },
     api_key: { type: Buffer },
     updated: { type: Date },
     created: { type: Date, default: new Date(), required: true }
