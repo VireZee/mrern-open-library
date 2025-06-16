6359a553868238528e0457d8d485381b590174b8
Vire <56460839+VireZee@users.noreply.github.com>
2025-06-16 15:59:19 +0700

    google credentials

--- server/src/routes/router.ts
+++ server/src/routes/router.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import passport from '@config/passport.ts'
 import apiController from '@controller/booksParent.ts'
 import verifyController from '@controller/verify.ts'
 
@@ -6,6 +7,19 @@ const router = Router({
     caseSensitive: true,
     strict: true
 })
+router.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }))
+// router.get('/auth/google/callback',
+//     passport.authenticate('google', {
+//         session: false,
+//         failureRedirect: '/login'
+//     }),
+//     (req, res) => {
+//         const user = req.user
+//         const token = generateJwt(user)
+//         res.cookie('!', token, { httpOnly: true, sameSite: 'lax' })
+//         res.redirect(`http://${process.env['DOMAIN']}:${process.env['CLIENT_PORT']}`)
+//     }
+// )
 router.get('/api/:token', apiController)
 router.get('/verify/:id/:token', verifyController)
 export default router
