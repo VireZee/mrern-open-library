24caf4c1fc0e6321558a34b3fca59167c8509ed1
Vire <56460839+VireZee@users.noreply.github.com>
2025-07-09 11:27:13 +0700

    reg button

--- client/src/components/auth/Register.tsx
+++ client/src/components/auth/Register.tsx
@@ -122,7 +122,13 @@ const Register: FC = () => {
                         </div>
                     )}
                     <button type="submit" className="w-full bg-black text-white py-2 px-4 my-1 rounded-md" disabled={loading}>{loading ? 'Loading...' : 'Register'}</button>
-                    <button onClick={() => location.href = `http://${import.meta.env['VITE_DOMAIN']}:${import.meta.env['VITE_SERVER_PORT']}/auth/google/register`} className="w-full bg-black text-white py-2 px-4 rounded-md">Register with Google</button>
+                    <button type="button" onClick={() => {
+                        const width = 500
+                        const height = 600
+                        const left = window.screenX + (window.outerWidth - width) / 2
+                        const top = window.screenY + (window.outerHeight - height) / 2
+                        window.open(`http://${import.meta.env['VITE_DOMAIN']}:${import.meta.env['VITE_SERVER_PORT']}/auth/google/register`, '_blank', `width=${width},height=${height},left=${left},top=${top}`)
+                    }} className="w-full bg-black text-white py-2 px-4 rounded-md">Register with Google</button>
                 </form>
                 <div className="mt-4 text-sm text-gray-700 text-center">
                     Already have an account? <a href="/login" className="font-medium text-black hover:text-black">Log In</a>
