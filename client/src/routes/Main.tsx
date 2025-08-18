204960554747226acb224189a39b82a21f598199
Vire <56460839+VireZee@users.noreply.github.com>
2025-08-18 10:41:25 +0700

    .

--- client/src/routes/Main.tsx
+++ client/src/routes/Main.tsx
@@ -1,5 +1,5 @@
 import { Navigate, Outlet } from 'react-router-dom'
-export default ({ user, verified }: { user: undefined | null, verified: boolean | null }) => {
+export default ({ user, verified }: { user: undefined | null, verified: null | boolean }) => {
     if (user === undefined && verified === null) return null
     if (verified === false) return <Navigate to='/verify' replace />
     return <Outlet />
